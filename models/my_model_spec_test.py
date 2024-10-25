02a313bf1d3f95d0ac3df715c8b0f9f936dc040b
Ryan-A-V <vieiraryan137@gmail.com>
2024-10-25 09:15:45 -0600

    Update my_model_spec_test.py

--- models/my_model_spec_test.py
+++ models/my_model_spec_test.py
@@ -66,13 +66,13 @@ class Autoencoder(Model):
         plt.plot(true_wave)
         plt.savefig(f"figs/{self.model_name}/Model_{self.model_name}_{self.latent_dim}_realwave.png")
         
-        sf.write('ogoutput.flac', true_wave, 1600)
+        sf.write(f"figs/{self.model_name}/Model_{self.model_name}_{self.latent_dim}_og_audio.flac", true_wave, 1600)
 
         plt.plot(modelYwave)
         plt.savefig(f"figs/{self.model_name}/Model_{self.model_name}_{self.latent_dim}_wave.png")
         fig, axs = plt.subplots(2,1)
 
-        sf.write('output.flac', modelYwave, 1600)
+        sf.write(f"figs/{self.model_name}/Model_{self.model_name}_{self.latent_dim}_output.flac", modelYwave, 1600)
 
         
         # axs[0].imshow(modelYwave, origin='lower', aspect='auto',
